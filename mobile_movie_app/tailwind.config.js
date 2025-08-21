5ea4d83e5509d4acae82c72df2eec95296cd3f82
Cha_mamme <solomonchamamme@gmail.com>
2025-08-21 03:42:43 +0000

    feat: added custom styles to the tailwind config

--- mobile_movie_app/tailwind.config.js
+++ mobile_movie_app/tailwind.config.js
@@ -1,10 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
-  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: '#030014',
+        secondary: '#151312',
+        accent: '#AB8BFF',
+        light:{
+          100: '#D6c6ff',
+          200: '#A8B5DB',
+          300: '#9CA4AB'
+        },
+        dark:{
+          100: '#221F3D',
+          200: '#0F0D23'
+        }
+
+      }
+    },
   },
   plugins: [],
 }
