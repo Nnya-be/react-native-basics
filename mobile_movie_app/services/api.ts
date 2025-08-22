1a247b049062fc73a9296a0c137873a285ae7ecc
Cha_mamme <solomonchamamme@gmail.com>
2025-08-22 15:43:06 +0000

    feat: rendring of the movies

--- mobile_movie_app/services/api.ts
+++ mobile_movie_app/services/api.ts
@@ -1,4 +1,3 @@
-
 export const TMDB_CONFIG ={
     BASE_URL: 'https://api.themoviedb.org/3',
     API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
@@ -7,6 +6,7 @@ export const TMDB_CONFIG ={
         Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_ACCESS_TOKEN}`
     }
 }
+
 
 export const fetchMovies = async ({query}: {query: string}) =>{
     const endpoint = query 
@@ -18,7 +18,7 @@ export const fetchMovies = async ({query}: {query: string}) =>{
         headers: TMDB_CONFIG.headers
     })
 
-    if( !response.ok){
+    if(!response.ok){
         //@ts-ignore
         throw new Error("Failed to fetch movies", response.statusText)
     }
@@ -26,3 +26,16 @@ export const fetchMovies = async ({query}: {query: string}) =>{
     const data = await response.json();
     return data.results;
 }
+// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NThjYWIwNWMyNmVjMjM1YmExZGM0ZWY5NjY0NTc5MiIsIm5iZiI6MTc1NTg2NDM0MS4xMDIwMDAyLCJzdWIiOiI2OGE4NWQxNTFmNTZmMjY1MDE2MGRiMGUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.QsqdQhMSN8YdPedray_SHncyKFlgTWZ_xywzdffnUvs'
+//   }
+// };
+
+// fetch(url, options)
+//   .then(res => res.json())
+//   .then(json => console.log(json))
+//   .catch(err => console.error(err));
