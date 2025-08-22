043da0150e7487b26ea6b1a110f5cad59cf9fd2d
Cha_mamme <solomonchamamme@gmail.com>
2025-08-22 13:17:49 +0000

    fix: secret key pushed

--- mobile_movie_app/services/api.ts
+++ mobile_movie_app/services/api.ts
@@ -26,16 +26,3 @@ export const fetchMovies = async ({query}: {query: string}) =>{
     const data = await response.json();
     return data.results;
 }
-// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NThjYWIwNWMyNmVjMjM1YmExZGM0ZWY5NjY0NTc5MiIsIm5iZiI6MTc1NTg2NDM0MS4xMDIwMDAyLCJzdWIiOiI2OGE4NWQxNTFmNTZmMjY1MDE2MGRiMGUiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.QsqdQhMSN8YdPedray_SHncyKFlgTWZ_xywzdffnUvs'
-//   }
-// };
-
-// fetch(url, options)
-//   .then(res => res.json())
-//   .then(json => console.log(json))
-//   .catch(err => console.error(err));
